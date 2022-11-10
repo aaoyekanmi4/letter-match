73f08dfa7590d037053c4b089424c5a2731a65bd
Arit Oyekan <aaoyekanmi4@gmail.com>
2022-11-09 19:53:00 -0600

    initial commit for letter match game

---
+++ data.js
@@ -0,0 +1,48 @@
+const pictureData = [
+  {
+    image: './img/apple.svg',
+    text: "Apple"
+  },
+  {
+    image: './img/bear.svg',
+    text: "Bear"
+  },
+  {
+    image: './img/cat.svg',
+    text: "Cat"
+  },
+  {
+    image: './img/dog.svg',
+    text: "Dog"
+  },
+  {
+    image: './img/elephant.svg',
+    text: "Elephant"
+  },
+  {
+    image: './img/fish.svg',
+    text: "Fish"
+  },
+  {
+    image: './img/grapes.svg',
+    text: "Grapes"
+  },
+  {
+    image: './img/house.svg',
+    text: "House"
+  },
+];
+
+const letterData = [
+    { text: "Aa" },
+    { text: "Bb" },
+    { text: "Cc" },
+    { text: "Dd" },
+    { text: "Ee" },
+    { text: "Ff" },
+    { text: "Gg" },
+    { text: "Hh" },
+]
+
+
+
---
+++ script.js
@@ -0,0 +1,16 @@
+const main = document.querySelector('main')
+
+const letterCardHTML = `<div class='card'>
+<div class="inner-card">
+  <div class="card-background">
+    <p>
+      ${data.question}
+    </p>
+  </div>
+  <div class="revealed-card">
+    <p>
+      ${data.answer}
+    </p>
+  </div>
+</div>
+</div>`
