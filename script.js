bfe4c812fb07fbd29dc4f8f67d522940d78f7907
Arit Oyekan <aaoyekanmi4@gmail.com>
2022-11-11 19:26:05 -0600

    refactored createCard

--- script.js
+++ script.js
@@ -4,40 +4,28 @@ const message = new SpeechSynthesisUtterance();
 const success = new Audio('/sounds/success-sound.mp3');
 const gameWon = new Audio('/sounds/Tada-sound.mp3');
 
-const letterCards = [];
-const wordCards = [];
+const allCards = []
 const lastRevealedCards = [];
 
-//Fisher Yates shuffle
-const shuffle = (array) => {
-  let j = 0
-  let temp = null;
-
-  for (let i = array.length - 1; i > 0; i -= 1) {
-    j = Math.floor(Math.random() * (i + 1));
-    temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
-};
-
-// set text for speech
+// Speech functions
 function setTextMessage (text) {
   message.text = text;
 }
 
-//speak text
+
 function speakText () {
   speechSynthesis.speak(message)
 }
 
+const readMessage = (data) => {
+  typeof data === 'string' ? setTextMessage(data[0]) : setTextMessage(data.text)
+  setTimeout(speakText, 400)
+}
+// Match Functions
 const lettersMatch = () => {
-  const comparisonTexts= []
-  lastRevealedCards.forEach((card) => {
-    const text = card.querySelector('p').innerText
-    comparisonTexts.push(text)
-  })
-  return  comparisonTexts[0][0] === comparisonTexts[1][0]
+  const cardOneFirstLetter = lastRevealedCards[0].querySelector('p').innerText[0]
+  const cardTwoFirstLetter = lastRevealedCards[1].querySelector('p').innerText[0]
+  return  cardOneFirstLetter === cardTwoFirstLetter
 }
 
 const checkForMatch = () => {
@@ -68,89 +56,80 @@ const checkForMatch = () => {
 const checkForGameOver = () => {
   const matchedCards = document.querySelectorAll('.matched')
   const cards = document.querySelectorAll('.card')
-  console.log(cards)
   if (matchedCards.length === cards.length) {
     gameWon.play()
   }
 }
 
-const createLetterCard = (letter) => {
-  const card = document.createElement('div');
-  card.classList.add('card', 'clickable')
-
-  card.innerHTML = `
-            <div class="inner-card">
-                <div class="inner-card-front">
-                    <img src="/img/card-background.svg" height="100%" />
-                </div>
-                <div class="inner-card-back letter">
-                    <p>
-                        ${letter}
-                    </p>
-                </div>
-            </div>
-`
-  card.addEventListener('click', () => {
-    if (card.classList.contains('clickable')) {
-      card.classList.remove('clickable')
-      card.classList.toggle('revealed')
-      setTextMessage(letter[0])
-       setTimeout(speakText, 400)
-      lastRevealedCards.push(card)
-      if (lastRevealedCards.length === 2) checkForMatch()
-         setTimeout(() => {
-        checkForGameOver()
-      }, 2000)
-    }
-  })
-
-  letterCards.push(card)
-}
-
-const createWordCard = (data) => {
-  const card = document.createElement('div');
-  card.classList.add('card', 'clickable')
-
-  const { text, image } = data
-  const firstLetter = text[0]
-  const remaningLetters = text.slice(1)
-  card.innerHTML = `
-  <div class="inner-card">
-                <div class="inner-card-front">
-                    <img src="/img/card-background.svg" height="100%" />
-                </div>
-                <div class="inner-card-back picture">
+// Create Card functions
+const createInnerCardHTML = (data) => {
+    if (typeof data === "object") {
+        const { text, image } = data
+        const firstLetter = text[0]
+        const remaningLetters = text.slice(1)
+        return `<div class="inner-card-back picture">
                     <img class="pic" src="${image}"/>
                     <p>
                         <span class="first">${firstLetter}</span>
                         <span class="rest">${remaningLetters}</span>
                     </p>
+                </div>`
+    } else {
+        return `<div class="inner-card-back letter">
+                    <p>
+                        ${data}
+                    </p>
+                </div>`
+    }
+}
+
+const createCard = (data) => {
+  const card = document.createElement('div');
+  card.classList.add('card', 'clickable')
+
+  card.innerHTML = `
+  <div class="inner-card">
+                <div class="inner-card-front">
+                    <img class='card-background' src="/img/card-background1.svg" />
                 </div>
+                   ${createInnerCardHTML(data)}
             </div>
 `
-
   card.addEventListener('click', () => {
     if (card.classList.contains('clickable')) {
       card.classList.remove('clickable')
       card.classList.toggle('revealed')
-      setTextMessage(text)
-      setTimeout(speakText, 400)
+      readMessage(data)
       lastRevealedCards.push(card)
 
+      // Game Flow Checks
       if (lastRevealedCards.length === 2) checkForMatch()
+
       setTimeout(() => {
         checkForGameOver()
       }, 2000)
     }
   })
 
-  wordCards.push(card)
+  allCards.push(card)
 }
 
-letterData.forEach((letter) => createLetterCard(letter))
-wordData.forEach((word) => createWordCard(word))
+// Randomize Cards
+const shuffle = (array) => {
+  let j = 0
+  let temp = null;
 
-const allCards = [...letterCards, ...wordCards]
+  for (let i = array.length - 1; i > 0; i -= 1) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+};
+
+letterData.forEach((letter) => createCard(letter))
+wordData.forEach((word) => createCard(word))
+
 shuffle(allCards)
 allCards.forEach((card) => main.appendChild(card))
 
